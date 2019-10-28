84f535caf3f61267459156d9c8b6811acd800059
liushengli <liujialong886@outlook.com>
2019-10-28 18:10:00 +0800

    init

---
+++ src/main/java/com/vike/agent/agent/AgentApplication.java
@@ -0,0 +1,16 @@
+package com.vike.agent.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @Author: lsl
+ * @CreateDate: 2019/10/28
+ */
+@SpringBootApplication
+public class AgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentApplication.class, args);
+    }
+
+}
